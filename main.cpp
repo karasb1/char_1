091469c385b575a3e1e18569435ce8ad9e237430
karasb1 <bohdankaras456@gmail.com>
2024-11-02 14:32:07 +0200

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-
+	char myChar[] = "Zoos are hugely popular attractions for adults and children alike.";
+	cout << myChar << endl;
+	for (int i = 0; i < sizeof(myChar); i++)
+	{
+		if (myChar[i] == 32)
+		{
+			myChar[i] = 9;
+		}
+	}
+	cout << myChar;
 	return 0;
 }
