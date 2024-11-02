1c8a13fbd76b7413186ac2c18e71a5d1a249fb22
karasb1 <bohdankaras456@gmail.com>
2024-11-02 17:25:29 +0200

    The ex4 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-
+	char myChar[100];
+	cin >> myChar;
+	for (int i = 0; i <= sizeof(myChar); i++)
+	{
+		if (myChar[i] == 46)
+		{
+			myChar[i] = 33;
+		}
+	}
+	cout << myChar;
 	return 0;
 }
