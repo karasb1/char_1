fd0aced101e4c0c35bf554cd1386a7bd4f188153
karasb1 <bohdankaras456@gmail.com>
2024-11-02 17:15:40 +0200

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main()
 {
-
+	char myChar[] = "qwer kdooopfd 1412 n(**41 412$ 14";
+	char resultChar[] = "";
+	cout << myChar << endl;
+	char symbol[] = "q";
+	for (int i = 0; i < sizeof(myChar); i++)
+	{
+		if (myChar[i] == symbol[0])
+		{
+			continue;
+		}
+		else
+		{
+			resultChar[i] = myChar[i];
+		}
+	}
+	cout << resultChar;
 	return 0;
 }
