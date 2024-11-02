c86ff8b8eaaf11c5d53d1603c448e0ef8fb1915b
karasb1 <bohdankaras456@gmail.com>
2024-11-02 17:29:20 +0200

    The ex5 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
-
+	char myChar[100];
+	char symbol[2];
+	cin >> myChar;
+	cin >> symbol;
+	int count = 0;
+	for (int i = 0; i <= sizeof(myChar); i++)
+	{
+		if (myChar[i] == symbol[0])
+		{
+			count++;
+		}
+	}
+	cout << count;
 	return 0;
 }
