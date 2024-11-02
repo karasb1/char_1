f249e9c220124e0df86336fa2157b4a789c8d3bc
karasb1 <bohdankaras456@gmail.com>
2024-11-02 14:40:29 +0200

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main()
 {
-
+	char myChar[] = "Zoos1! are2@ hugely3# popular4$ attractions5% for6^ adults7& and8* children9( alike10).";
+	int letter = 0;
+	int numbers = 0;
+	int other_symbols = 0;
+	for (int i = 0; i <= sizeof(myChar); i++)
+	{
+		if (myChar[i] > 47 && myChar[i] < 58)
+		{
+			numbers++;
+		}
+		else if (myChar[i] > 65 && myChar[i] < 91 || myChar[i] > 96 && myChar[i] < 123)
+		{
+			letter++;
+		}
+		else
+		{
+			other_symbols++;
+		}
+	}
+	cout << "Letter = " << letter << endl;
+	cout << "Numbers = " << numbers << endl;
+	cout << "Other symbols = " << other_symbols << endl;
 	return 0;
 }
