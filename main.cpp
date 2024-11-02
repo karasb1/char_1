65f01f8f62b07ceea568e55945b01595a5f2200f
karasb1 <bohdankaras456@gmail.com>
2024-11-02 17:21:55 +0200

    The ex3 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void r(char myChar[], int a, const char b[])
+{
+	myChar[a] = b[0];
+	cout << myChar << endl;
+}
+
+
 int main()
 {
-
+	char mychar[] = "My char";
+	cout << mychar << endl;
+	r(mychar, 3, "a");
 	return 0;
 }
