44e87cfd8e353c566fb177facda5eca6aaaa1650
karasb1 <bohdankaras456@gmail.com>
2024-11-02 17:10:00 +0200

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
-
+	char myChar[] = "Zoos";
+	cout << myChar << endl;
+	int number = 2;
+	for (int i = 0; i < sizeof(myChar); i++)
+	{
+		if (i == number)
+		{
+			continue;
+		}
+		else
+		{
+			cout << myChar[i];
+		}
+	}
 	return 0;
 }
